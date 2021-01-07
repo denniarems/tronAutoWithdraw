f5ee7dab9a5253d6445d1f1eb6c54e54b439870c
denniarems <denniarems@gmail.com>
2021-01-07 23:41:53 +0530

    🐛 FIX:

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const TronWeb = require('tronweb');
-
+const TronWeb = require("tronweb");
+require("dotenv").config();
 function getBTTBalance(asset) {
-  return asset.key === '1002000';
+  return asset.key === "1002000";
 }
 async function app(to, from, privatekey) {
   const tronWeb = new TronWeb(
     {
-      fullNode: 'https://api.trongrid.io',
-      solidityNode: 'https://api.trongrid.io',
-      eventServer: 'https://api.trongrid.io',
+      fullNode: "https://api.trongrid.io",
+      solidityNode: "https://api.trongrid.io",
+      eventServer: "https://api.trongrid.io",
     },
     privatekey
   );
@@ -18,22 +18,19 @@ async function app(to, from, privatekey) {
     const data = await tronWeb.transactionBuilder.sendToken(
       to,
       bttBalance,
-      '1002000',
+      "1002000",
       from
     );
     const signedtxn = await tronWeb.trx.sign(data, privatekey);
     await tronWeb.trx.sendRawTransaction(signedtxn);
   }
 }
-const to = process.env.TO 
-const froms = (process.env.FROM).split(',');
-const keys = (process.env.KEY).split(',');
+console.log(process.env);
+const to = process.env.TO;
+const froms = process.env.FROM.split(",");
+const keys = process.env.KEY.split(",");
 for (const [i, key] of keys.entries()) {
   const interval = setInterval(function () {
-    app(
-      to,
-      froms[i],
-      key
-    );
+    app(to, froms[i], key);
   }, 10000);
 }
